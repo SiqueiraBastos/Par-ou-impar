7c1f05158a6a100c62cf0742e281139588d3737d
SiqueiraBastos <siqueira.bastos@hotmail.com>
2022-01-25 15:40:31 -0300

    par ou impar

---
+++ main.c
@@ -0,0 +1,32 @@
+/* Programa para saber se é par ou impar
+
+por José de Ribamar Siqueira Bastos
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+
+    printf("Entre com o valor de x:  ");
+    scanf("%d", &x);
+
+    if (x %2== 0)
+        printf("par");
+
+        else
+            printf("impar");
+
+     return 0;
+
+
+
+
+
+
+
+
+}
